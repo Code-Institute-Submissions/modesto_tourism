f136ffde48e82103f6b9e95ae87d92cdee6d95e0
Derek <thestarvingcoder@gmail.com>
2019-05-14 14:01:16 -0700

    added weather widget, resized images

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,33 +1,4 @@
-      function weatherData(data) {
-          var apiCall = "api.openweathermap.org/data/2.5/weather?q=Modesto&units=imperial&APPID=e5907fb3d33363041fad1e9d19229350";
-          console.log(apiCall);
-          var icon = "https://openweathermap.org/img/w" + data.weather[0].icon + ".png";
-          var temp = Math.floor(data.main.temp);
-          var weather = data.weather[0].main;
-          $(".icon").attr("src", icon);
-          $(".weather").append(weather);
-          $(".temp").append(temp);
-      }
+window.myWidgetParam ? window.myWidgetParam : window.myWidgetParam = [];  window.myWidgetParam.push({id: 15,cityid: '5373900',appid: 'e5907fb3d33363041fad1e9d19229350',units: 'imperial',containerid: 'openweathermap-widget-15',  });  (function() {var script = document.createElement('script');script.async = true;script.charset = "utf-8";script.src = "//openweathermap.org/themes/openweathermap/assets/vendor/owm/js/weather-widget-generator.js";var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(script, s);  })();
 
-      //      $.getJSON(apiCall, weatherCallback);
-      //      var apiCall = "api.openweathermap.org/data/2.5/weather?q=Modesto&units=imperial&APPID=e5907fb3d33363041fad1e9d19229350";
-      //       function weatherCallback(weatherData){
-      //           var cityName = weatherData.name;
-      //           var country = weatherData.sys.country;
-      //           var description = weatherData.weather[0].description;
-      //           var icon = "https://openweathermap.org/img/w" + data.weather[0].icon + ".png";
-      //           $(".weatherResponse").append ("The weather in " + cityName + " " + country + "is currently" + description + icon);
-      //       };
-
-      // // "api.openweathermap.org/data/2.5/weather?q=Modesto&units=imperial&APPID=e5907fb3d33363041fad1e9d19229350",
-
-      // function(data){
-      //     console.log(data);
-      //     var icon = "https://openweathermap.org/img/w" + data.weather[0].icon + ".png";
-      //     var temp = Math.floor(data.main.temp);
-      //     var weather = data.weather[0].main;
-
-      //     $(".icon").attr("src", icon);
-      //     $(".weather").append(weather);
-      //     $(".temp").append(temp);
-      // }
+// changes div class from right to center
+$('.widget-right', 'weather-right--type1').toggleClass('widget-right', 'weather-right--type1');
