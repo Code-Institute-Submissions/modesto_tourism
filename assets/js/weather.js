c426a7ba574b2001ecec7e319297ebf8cd5ed1d3
Derek <thestarvingcoder@gmail.com>
2019-05-13 17:06:46 -0700

    weather edits

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,44 +1,33 @@
-const baseURL = "api.openweathermap.org/data/2.5/weather?q=Modesto&units=imperial&APPID=e5907fb3d33363041fad1e9d19229350";
+      function weatherData(data) {
+          var apiCall = "api.openweathermap.org/data/2.5/weather?q=Modesto&units=imperial&APPID=e5907fb3d33363041fad1e9d19229350";
+          console.log(apiCall);
+          var icon = "https://openweathermap.org/img/w" + data.weather[0].icon + ".png";
+          var temp = Math.floor(data.main.temp);
+          var weather = data.weather[0].main;
+          $(".icon").attr("src", icon);
+          $(".weather").append(weather);
+          $(".temp").append(temp);
+      }
 
-function getData(type, cb) {
-    var xhr = new XMLHttpRequest();
+      //      $.getJSON(apiCall, weatherCallback);
+      //      var apiCall = "api.openweathermap.org/data/2.5/weather?q=Modesto&units=imperial&APPID=e5907fb3d33363041fad1e9d19229350";
+      //       function weatherCallback(weatherData){
+      //           var cityName = weatherData.name;
+      //           var country = weatherData.sys.country;
+      //           var description = weatherData.weather[0].description;
+      //           var icon = "https://openweathermap.org/img/w" + data.weather[0].icon + ".png";
+      //           $(".weatherResponse").append ("The weather in " + cityName + " " + country + "is currently" + description + icon);
+      //       };
 
-    xhr.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            cb(JSON.parse(this.responseText));
-        }
-    };
+      // // "api.openweathermap.org/data/2.5/weather?q=Modesto&units=imperial&APPID=e5907fb3d33363041fad1e9d19229350",
 
-    xhr.open("GET", baseURL + type + "/");
-    xhr.send();
-}
+      // function(data){
+      //     console.log(data);
+      //     var icon = "https://openweathermap.org/img/w" + data.weather[0].icon + ".png";
+      //     var temp = Math.floor(data.main.temp);
+      //     var weather = data.weather[0].main;
 
-function writeToDocument(type) {
-    var el = document.getElementById("data");
-    el.innerHTML = "";
-
-    getData(type, function(data) {
-        data = data.results;
-
-        data.forEach(function(item) {
-            el.innerHTML += "<p>" + item.name + "</p>";
-        });
-    });
-}
-
-
-$().getJSON
-{
-      "api.openweathermap.org/data/2.5/weather?q=Modesto&units=imperial&APPID=e5907fb3d33363041fad1e9d19229350",
-
-    function(data){
-        console.log(data);
-        var icon = "https://openweathermap.org/img/w" + data.weather[0].icon + ".png";
-        var temp = Math.floor(data.main.temp);
-        var weather = data.weather[0].main;
-
-        $(".icon").attr("src", icon);
-        $(".weather").append(weather);
-        $(".temp").append(temp);
-    }
-};
+      //     $(".icon").attr("src", icon);
+      //     $(".weather").append(weather);
+      //     $(".temp").append(temp);
+      // }
